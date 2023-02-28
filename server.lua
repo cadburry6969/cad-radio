6376e24c348ca940cedce4f7bebc50ccb0d134f5
cadburry <archxcadburry@gmail.com>
2023-02-28 21:54:20 +0530

    member list display fix

--- server.lua
+++ server.lua
@@ -3,14 +3,20 @@ local QBCore = exports["qb-core"]:GetCoreObject()
 local RadioList = {}
 
 RegisterNetEvent('radio:addToList', function(radio, name)
-    if not radio or not name then return end
+    local source = source
+    if not radio then return end
+    local Player = QBCore.Functions.GetPlayer(source)
+    local name = tostring(Player.PlayerData.charinfo.firstname.." "..Player.PlayerData.charinfo.lastname)
     if RadioList[radio] == nil then RadioList[radio] = {} end
     RadioList[radio][#RadioList[radio]+1] = name
 end)
 
 RegisterNetEvent('radio:removeFromList', function(radio, name)
-    if not radio or not name then return end
+    local source = source
+    if not radio then return end
     if RadioList[radio] == nil then return end
+    local Player = QBCore.Functions.GetPlayer(source)
+    local name = tostring(Player.PlayerData.charinfo.firstname.." "..Player.PlayerData.charinfo.lastname)
     for i=1, #RadioList[radio] do
         if name == RadioList[radio][i] then
             table.remove(RadioList[radio], i)
